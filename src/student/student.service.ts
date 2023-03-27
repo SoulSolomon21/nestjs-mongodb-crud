7bc2f04dfbdb7fda8026850a1fc5946567b58057
Soul_Solomon <sekamattesoul@gmail.com>
2023-03-27 08:58:33 +0300

    created crud operations in the StudentService class

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -6,7 +6,10 @@ import { UpdateStudentDto } from 'src/dto/update-student.dto';
 import { IStudent } from 'src/interface/student.interface';
 
 @Injectable()
+// this decorator means we can inject the service class into other classes using the principles of dependency injection
 export class StudentService {
+  // in the constructor, we inject the studentModel into the service using the InjectModel decorator
+  // this injection is only possible after the schema is registered in the app module configuration
   constructor(@InjectModel('Student') private studentModel: Model<IStudent>) {}
 
   async createStudent(createStudentDto: CreateStudentDto): Promise<IStudent> {
@@ -30,7 +33,30 @@ export class StudentService {
     return existingStudent;
   }
 
-async
+  async getAllStudents(): Promise<IStudent[]> {
+    const studentData = await this.studentModel.find();
 
+    if (!studentData || studentData.length == 0) {
+      throw new NotFoundException('Students data not found!');
+    }
+    return studentData;
+  }
 
+  async getStudent(studentId: string): Promise<IStudent> {
+    const existingStudent = await this.studentModel.findById(studentId).exec();
+
+    if (!existingStudent) {
+      throw new NotFoundException(`Student #${studentId} not found`);
+    }
+    return existingStudent;
+  }
+
+  async deleteStudent(studentId: string): Promise<IStudent> {
+    const deletedStudent = await this.studentModel.findByIdAndDelete(studentId);
+
+    if (!deletedStudent) {
+      throw new NotFoundException(`Student #${studentId} not found`);
+    }
+    return deletedStudent;
+  }
 }
