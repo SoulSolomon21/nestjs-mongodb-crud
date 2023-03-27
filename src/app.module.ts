ca0740a09213f6cd2239676de13b5bf4619ac1a9
Soul_Solomon <sekamattesoul@gmail.com>
2023-03-27 09:58:22 +0300

    completed app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StudentSchema } from './schema/student.schema';
 import { StudentService } from './service/student/student.service';
-import { StudentController } from './controller/student/student.controller';
+import StudentController from './controller/student/student.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017', {
       dbName: 'studentdb',
     }),
     MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
